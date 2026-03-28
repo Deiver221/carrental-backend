c879f31e59e82dc4454f4ebe77c89d93dc9b6116
Deiverrr <delvin2812@gmail.com>
2026-03-28 16:55:56 -0300

    Welcome blade DELETED

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'CarRental API',
+        'status' => 'running'
+    ]);
 });
 
